99d3b5c03cc76b769ae8503629a965fbebcee882
webferrol <xurxo@webferrol.com>
2021-12-03 13:07:55 +0100

    Arreglado js

--- js/app.js
+++ js/app.js
@@ -4,18 +4,3 @@
  * document.createTextNode() --> creación de nodos de tipo object node text
  * element.className = 'nombre-clase' // element.setAttribute('class', 'nombre-clase') --> poner nombre clase de un atributo // element.classList.add('nombre-clase)
  */
-
-
-
-
-//Vamos a evitar que los formularios puedan ser enviados. Una opción es cogerlos a los dos
-const formularios = document.querySelectorAll("form");
-//A continuación los pasamos por un bucle y capturamos el evento submit
-formularios.forEach(
-    form => {
-        console.log(form)
-        form.onsubmit = e => {
-            e.preventDefault();
-        }
-    }
-);
